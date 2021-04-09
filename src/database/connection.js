28a3f2712cd783b6ac0ffdd074b883ddf08f5a71
Celestino <andersoncelestino.n@gmail.com>
2021-04-09 15:18:00 -0300

    Creating a database connection

---
+++ src/database/connection.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+
+const getConnection = async () => {
+    try {
+        return await mongoose.createConnection('mongodb://localhost/JWT_Auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        })
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
+
+
+getConnection()
+    .then(res => {
+        console.log(res)
+    })
