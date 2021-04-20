0538592babc4e890d2430bb8f4f1dd9783a516db
Celestino <andersoncelestino.n@gmail.com>
2021-04-20 09:40:52 -0300

    Delete validateRegisterCredentials.js

--- src/middlewares/register/validateRegisterCredentials.js
+++
@@ -1,9 +0,0 @@
-const validateRegisterCredentials = async (req, res, next) => {
-    try {
-        
-    } catch (error) {
-        return res.status(409).json({
-            error: "Credentials invalid."
-        })
-    }
-}
