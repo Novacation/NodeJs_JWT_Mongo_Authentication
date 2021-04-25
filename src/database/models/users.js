99be698129f3fb9aea57a2e72e3ed3bac49fe9c8
Celestino <andersoncelestino.n@gmail.com>
2021-04-25 01:55:00 -0300

    Update users.js

--- src/database/models/users.js
+++ src/database/models/users.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose")
-const schema = new mongoose.Schema({
+
+const usersSchema = new mongoose.Schema({
     login: {
         type: 'string',
         unique: true,
         required: true
-    }, 
+    },
     password: {
         type: 'string',
         unique: false,
@@ -12,13 +13,47 @@ const schema = new mongoose.Schema({
     }
 })
 
-const user = mongoose.model('users', schema)
+const userModel = mongoose.model('users', usersSchema)
 
-const newUser = new user({
-    login: "myLogin", 
-    password: "myPassword"
-})
 
-newUser.save((err)=>{
-    if(err) return false
-})
+
+class User {
+    #login; 
+    #password;
+
+    constructor(login, password) {
+        this.#setLogin(login)
+        this.#setPassword(password)
+    }
+
+    static createUser(user) {
+        const newUser = new userModel({
+            login: user.getLogin,
+            password: user.getPassword
+        })
+
+        newUser.save()
+            .then(res =>{
+                console.log(res)
+            })
+    }
+
+    get getLogin() {
+        return this.#login
+    }
+
+    get getPassword() {
+        return this.#password
+    }
+
+    set #setLogin(login) {
+        this.#login = login
+    }
+
+    set #setPassword(password) {
+        this.#password = password
+    }
+}
+
+
+module.exports = User
