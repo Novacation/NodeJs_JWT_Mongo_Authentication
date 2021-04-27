7898511f9313b64374b31f3b4500eeb4e62ec38a
Celestino <andersoncelestino.n@gmail.com>
2021-04-27 12:06:29 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-const express = require("express")
-const app = express()
-const bodyParse = require("body-parser")
-const path = require("path")
+const app = require("./server")
 const getConnection = require("./database/connection")
-const authRouter = require("./routes/authentication")
-
-//Server config
-app.use(express.static(path.join(__dirname, '/src')))
-app.use(bodyParse.urlencoded({
-    extended: false
-}))
-app.use(bodyParse.json())
-
-
-// Routes
-
-app.use('/auth', authRouter)
-
 
 const PORT = 8000
 app.listen(PORT, async () => {
